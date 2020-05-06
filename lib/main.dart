d972b4d9643f24f3d43190c93ae3ffb1d7eb63af
Tim Dygert <tdygert@gmail.com>
2020-05-06 17:01:54 -0400

    played with column and row

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,48 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Containers',
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Containers',
+            ),
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.white,
+                child: Text(
+                  'Container 1',
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+                child: Text(
+                  'Container 2',
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Text(
+                  'Container 3',
+                ),
+              ),
+            ],
           ),
         ),
       ),
-      body: SafeArea(
-        child: Container(
-          height: 100,
-          width: 100,
-          margin: EdgeInsets.only(
-            left: 30,
-            top: 50,
-          ),
-          padding: EdgeInsets.all(
-            20,
-          ),
-          color: Colors.white,
-          child: Text(
-            'Hello',
-          ),
-        ),
-      ),
-    ));
+    );
   }
 }
